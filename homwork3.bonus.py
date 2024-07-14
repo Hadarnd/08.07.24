c11f82a5e7d85123fbdca34da87ae00f2dbf2010
Hadar <hadarnd8@gmail.com>
2024-07-14 21:23:08 +0300

    Add files via upload

---
+++ homwork3.bonus.py
@@ -0,0 +1,22 @@
+#start
+#question 1:
+
+name: str = input("what is your name?")
+height: float = float(input("what is your height?"))
+name2: str = input("what is your dog's name?")
+name3: str = input("what is your cat's name?")
+print(f"name: {name:<10}.")
+print(f"Name: {height:.4}")
+print(f"Name: {name2:^10}.")
+print(f"Name: {name3:^10}.")
+
+#for neat and clean reading:
+
+print(f"name: {name:^10}.")
+print(f"Name: {height:^10}.")
+print(f"Name: {name2:^10}.")
+print(f"Name: {name3:^10}.")
+
+
+
+#end
